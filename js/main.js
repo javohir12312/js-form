6078861f70c35e6c98e0a4dff8d2c8d98f743d3e
javohir12312 <abduxalilovjavohir393@gmail.com>
2022-07-30 19:37:47 +0300

    full

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var elForm = document.querySelector(".form");
+var speakName = document.querySelector(".input1");
+var speakMoney = document.querySelector(".input2");
+var money = 2000;
+
+var elP = document.querySelector(".text");
+
+elForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+
+  if (isNaN(speakMoney.value)) {
+    elP.textContent = 'Son kiriting'
+  } else if(speakMoney.value >= money){
+    elP.textContent = 'Otdingiz'
+  }
+})
